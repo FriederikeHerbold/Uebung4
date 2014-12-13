673328898ab7d11e220bea8310ca851d6ef434fc
Friederike <F.herbold@googlemail.com>
2014-12-13 18:07:10 +0100

    Factory gescgrieben JavaDoc fehlt noch

--- CrypterFactory.java
+++ CrypterFactory.java
@@ -4,7 +4,23 @@ public class CrypterFactory {
 	public enum Verschluesselung {
 		CAESAR, SUBSTITUTION, REVERSE, XOR, NULL;
 	}
-	public Verschluesselung createCrypter(String Schlüssel){
-		return Verschluesselung.NULL;
+	
+	public Crypter createCrypter(String schluessel, Verschluesselung art){
+		switch (art){
+		case CAESAR: 
+			return new CrypterCarsar(schluessel);
+		case SUBSTITUTION:
+			return new CrypterSubstitution(schluessel);
+		case REVERSE:
+			return new CrypterReverse(schluessel);
+		case XOR:
+			return new CrypterXor(schluessel);
+		case NULL:
+			return new CrypterNull(schluessel);
+		default:
+			return null;
+			
+		}
+		
 	}
 }
