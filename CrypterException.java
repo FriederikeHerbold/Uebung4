79d66d044a9b3fc6dfcaf4fca22b82dd4eeca9f5
Oksana <oksana.semeshkina@stud.hs-mannheim.de>
2014-12-14 16:30:12 +0100

    Konstruktor für Exception

--- CrypterException.java
+++ CrypterException.java
@@ -1,5 +1,16 @@
 package ws2014.tpe.gruppe_1415349_1410206.uebung4;
 
 public class CrypterException extends Exception {
+	private char symbol;
 
+	public CrypterException(char symbol) {
+		super();
+		this.symbol = symbol;
+
+	}
+
+	public char getSymbol() {
+		return this.symbol;
+
+	}
 }
