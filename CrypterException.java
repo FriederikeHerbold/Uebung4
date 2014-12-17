ee7d08ed1c3a11d0b14f17f8bebe0512051bab6f
Friederike <F.herbold@googlemail.com>
2014-12-17 09:45:31 +0100

    leerer konstruktor hinzugefügt

--- CrypterException.java
+++ CrypterException.java
@@ -3,6 +3,10 @@ package ws2014.tpe.gruppe_1415349_1410206.uebung4;
 public class CrypterException extends Exception {
 	private char symbol;
 
+	public CrypterException(){
+		
+	}
+	
 	public CrypterException(char symbol) {
 		super();
 		this.symbol = symbol;
